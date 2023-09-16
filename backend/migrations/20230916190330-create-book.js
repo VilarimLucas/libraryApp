15a3afb00617f1b19089dc38615334bc7e00a729
VilarimLucas <98033137+VilarimLucas@users.noreply.github.com>
2023-09-16 16:15:47 -0300

    models of the library

---
+++ backend/migrations/20230916190330-create-book.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fk_publishingcompany: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'PublishingCompanies', key: 'id'},
+        onDelete: 'CASCADE'
+      },
+      fk_category: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Categories', key: 'id'},
+        onDelete: 'CASCADE'
+      },
+      fk_author: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Authors', key: 'id'},
+        onDelete: 'CASCADE'
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Books');
+  }
+};
