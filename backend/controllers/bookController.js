ff06e36b6a495185cb13ff118239889c012c9489
VilarimLucas <98033137+VilarimLucas@users.noreply.github.com>
2023-09-17 10:51:24 -0300

    API REST testada

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -4,10 +4,19 @@ const router = express.Router();
 //pegamos a entidade em si dessa forma usando .Livro
 const Livro = require('../models').Book;
 
+// -- Esse é o INNER JOIN referente a VIEW data_books
+// select b.id, b.title, a.author_name, c.category_description, pc.publishing_company_description from books as b
+// inner join authors as a on a.id = b.fk_author
+// inner join categories as c on c.id = b.fk_category
+// inner join publishingcompanies as pc on pc.id = b.fk_publishingcompany;
+
 //Busca Livro (GET)
 router.get('/', async (req, res) => {
-    const livros = await Livro.findAll();
-    res.status(200).json(livros);
+    await Livro.sequelize.query("select * from data_books",
+        { model: Livro }).then(function (books) {
+            var nbooks = JSON.parse(JSON.stringify(books));
+            res.status(200).json(nbooks);
+        });
 });
 
 //Cadastra Livro (POST)
@@ -19,12 +28,32 @@ router.post('/', async (req, res) => {
     res.status(200).json({ message: 'Cadastrado com sucesso' });
 });
 
-//Busca Por id o Livro (GET)
+// Busca Livro por ID (GET)
 router.get('/:id', async (req, res) => {
-    const id = req.params;
-    const livro = await Livro.findByPk(req.params.id);
-    res.status(200).json(livro);
+    const livroId = req.params.id;
+
+    try {
+        // Consulta o livro pelo ID usando o Sequelize e a view data_books
+        const livro = await Livro.sequelize.query(
+            `SELECT * FROM data_books WHERE id = :livroId`,
+            {
+                replacements: { livroId },
+                type: Livro.sequelize.QueryTypes.SELECT,
+            }
+        );
+
+        // Verifica se o livro foi encontrado
+        if (!livro || livro.length === 0) {
+            return res.status(404).json({ message: 'Livro não encontrado' });
+        }
+
+        res.status(200).json(livro);
+    } catch (error) {
+        console.error('Erro ao buscar livro por ID:', error);
+        res.status(500).json({ message: 'Erro interno do servidor' });
+    }
 });
+
 
 //Deleta Livro por id (DELETE)
 router.delete('/:id', async (req, res) => {
@@ -39,7 +68,7 @@ router.delete('/:id', async (req, res) => {
 //Altera Livro por ID (PUT)
 router.put('/:id', async (req, res) => {
     const { fk_publishingcompany, fk_category, fk_author, title } = req.body;
-    await Autor.update(
+    await Livro.update(
         { fk_publishingcompany, fk_category, fk_author, title },
         {
             where: { id: req.params.id },
